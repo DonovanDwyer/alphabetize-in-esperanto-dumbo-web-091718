dd69c44843ca4e3dbc30dcfdf71d381b60102959
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-14 17:06:00 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,7 +4,8 @@ def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   array = arr
   #binding.pry
-  array = arr.sort_by! {|a,b| a.index {|x| }}
+  array = arr.sort_by! {|a| alphabet.index {|x| x.includes(a)}}
+  array
    # binding.pry
   #end
 end
