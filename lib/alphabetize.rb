081250e572a3a1b5e07ffd898aa4bc86ac566696
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-14 17:12:27 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,7 @@ require "pry"
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   array = arr
-  binding.pry
+  #binding.pry
   array = arr.sort {|a,b| alphabet.index(a[0]) <=> alphabet.index(b[0])}
   array
    # binding.pry
